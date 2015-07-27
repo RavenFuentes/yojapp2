73dd5be111073c3e2b69535adbe47ddc85b6201a
Joy Fementira <joyfementira@gmail.com>
2015-07-28 00:19:39 +0800

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,4 +1,5 @@
-$('.add-to-cart .more').click(function(){
+	//Add to cart extraction button
+	$('.add-to-cart .more').click(function(){
 		var newVal = parseInt($(this).closest('form').find('input[type="text"]').val()) + 1;
 		$(this).closest('form').find('input[type="text"]').val(newVal);
 	});
